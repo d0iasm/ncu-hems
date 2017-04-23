2d9717826b4c897e077d1cba4f3f7e573deba01a
Asami Doi <doiasami1219@gmail.com>
2017-04-23 20:46:17 +0900

    add: ejs

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,11 @@
 var gulp = require("gulp");
 var sass = require("gulp-sass");
+var ejs = require("gulp-ejs");
 var autoprefixer = require("gulp-autoprefixer");
 var uglify = require("gulp-uglify");
 var browser = require("browser-sync");
 var plumber = require("gulp-plumber");
+var rename = require("gulp-rename");
 
 gulp.task("server", function() {
     browser({
@@ -30,7 +32,17 @@ gulp.task("js", function() {
         .pipe(browser.reload({stream:true}))
 });
 
+gulp.task("ejs", function() {
+    gulp.src(["ejs/**/*.ejs",'!' + "ejs/**/_*.ejs"])
+        .pipe(plumber())
+        .pipe(ejs())
+        .pipe(rename({extname: '.html'}))
+        .pipe(gulp.dest("./"))
+        .pipe(browser.reload({stream:true}))
+});
+
 gulp.task("default", ['server'], function() {
-    gulp.watch(["js/**/*.js","!js/min/**/*.js"],["js"]);
     gulp.watch("sass/**/*.scss",["sass"]);
+    gulp.watch(["js/**/*.js","!js/min/**/*.js"],["js"]);
+    gulp.watch(["ejs/**/*.ejs",'!' + "ejs/**/_*.ejs"],["ejs"]);
 });
