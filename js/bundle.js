a22e58b0d75c75909ef1393ece53e6cfb766c2d7
Asami Doi <doiasami1219@gmail.com>
2017-04-29 16:17:59 +0900

    add: chart js library

--- js/bundle.js
+++ js/bundle.js
@@ -45,9 +45,33 @@
 /***/ (function(module, exports) {
 
 	(function() {
-	  //   window.addEventListener('load', function() {
-	  //       alert('loaded!!!');
-	  //  });
+	  var ctx = document.getElementById("power-usage");
+	  var data = {
+	    labels: [
+	        "Red",
+	        "Blue",
+	        "Yellow"
+	    ],
+	    datasets: [
+	        {
+	            data: [300, 50, 100],
+	            backgroundColor: [
+	                "#FF6384",
+	                "#36A2EB",
+	                "#FFCE56"
+	            ],
+	            hoverBackgroundColor: [
+	                "#FF6384",
+	                "#36A2EB",
+	                "#FFCE56"
+	            ]
+	        }]
+	  };
+	  var myPieChart = new Chart(ctx,{
+	    type: 'pie',
+	    data: data
+	    // options: options
+	  });
 	})();
 
 
