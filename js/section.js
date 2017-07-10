d40ea03201cccc25c40e92fbf0e7458a45fcf78b
Asami Doi <doiasami1219@gmail.com>
2017-07-11 08:47:20 +0900

    add remove section function

--- js/section.js
+++ js/section.js
@@ -1,4 +1,15 @@
-function editSection(){
+(function() {
+  // var hoge = document.getElementById("hoge");
+  // var menuItems =  document.querySelector('.menu-item');
+  var menuItems = document.getElementsByClassName('menu-item');
+  menuItems.addEventListener("click", menuClick(event), false);
+  // document.getElementById("hoge").addEventListener("click", function( event ) {
+  //   console.log("hoge");
+  //   event.target.textContent = "click count: " + event.detail;
+  // }, false);
+})
+
+function addSection(){
   var menu =  document.querySelector('.menu');
   var section = document.createElement('li');
   section.classList.add('menu-item');
@@ -6,9 +17,26 @@ function editSection(){
   menu.appendChild(section);
 }
 
-function menuClick(){
-  var menuItem =  document.querySelector('.menu-item');
-  menuItem.classList.remove('active');
-  menuItem.classList.add('active');
+function removeSection(e){
+  var currentMenuItem = e.target.parentNode;
+  currentMenuItem.parentNode.removeChild(currentMenuItem);
+}
+
+function removeAction(){
+  var menuItems =  document.querySelectorAll('.menu-item');
+  for(var i=0; i<menuItems.length; i++){
+    menuItems[i].classList.remove('active');
+  }
+}
+
+function menuClick(e){
+  // var menuItems =  document.querySelectorAll('.menu-item');
+  // menuItem.classList.remove('active');
+  // menuItem.classList.add('active');
+  // div.classList.toggle("visible");
+  // for(var i=0; i<menuItems.length; i++){
+  //   menuItems[i].classList.toggle('active');
+  // }
   console.log("hoge");
+  console.log(e.target);
 }
