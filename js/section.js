25d9aa59ffbbffb266edb3b36d6dc92ccb28ef15
Asami Doi <doiasami1219@gmail.com>
2017-07-11 09:06:08 +0900

    fix menu active toggle

--- js/section.js
+++ js/section.js
@@ -11,32 +11,34 @@
 
 function addSection(){
   var menu =  document.querySelector('.menu');
-  var section = document.createElement('li');
-  section.classList.add('menu-item');
-  section.textContent = 'hoge';
-  menu.appendChild(section);
+  var newSection = document.createElement('li');
+  newSection.classList.add('menu-item');
+  if(menu.childElementCount == 0){
+    newSection.classList.add('active');
+  }
+  newSection.textContent = 'hoge';
+  menu.appendChild(newSection);
 }
 
 function removeSection(e){
   var currentMenuItem = e.target.parentNode;
+  // TODO: 現在のactiveを消したら他の要素をactiveにする
+  // if(currentMenuItem.className = 'active'){
+  //   currentMenuItem.nextElementSibling.classList.add('active');
+  // }
   currentMenuItem.parentNode.removeChild(currentMenuItem);
 }
 
-function removeAction(){
+function addActive(e){
+  removeActive();
+  if(e.target.tagName == 'LI'){
+    e.target.classList.add('active');
+  }
+}
+
+function removeActive(){
   var menuItems =  document.querySelectorAll('.menu-item');
   for(var i=0; i<menuItems.length; i++){
     menuItems[i].classList.remove('active');
   }
-}
-
-function menuClick(e){
-  // var menuItems =  document.querySelectorAll('.menu-item');
-  // menuItem.classList.remove('active');
-  // menuItem.classList.add('active');
-  // div.classList.toggle("visible");
-  // for(var i=0; i<menuItems.length; i++){
-  //   menuItems[i].classList.toggle('active');
-  // }
-  console.log("hoge");
-  console.log(e.target);
 }
